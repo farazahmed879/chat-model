5ce164ec2b9d7bf30b1cad0707ee89092adf725c
Faraz Ahmed Shaikh <lets_play879@yahoo.com>
2025-03-18 17:37:31 +0500

    intial-2

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+
+const API_BASE_URL = process.env.VITE_API_BASE_URL || "http://localhost:4891";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      "/api/chat": {
+        target: API_BASE_URL,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/chat/, "/v1/chat/completions"),
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react()],
+})
